9b38b5b57777cd7aefc12657ac76c4ec477bfa36
nyerogude <ogudeoren@gmail.com>
2025-07-06 17:37:29 +0100

    refactor query:calculate marketing_roi_by_month.sql

--- marketing_roi_by_month.sql
+++ marketing_roi_by_month.sql
@@ -1,9 +1,9 @@
 -- Marketing ROI By Month
 SELECT 
   ms.month,
-  SUM(ms.amount_spent) AS total_marketing_spend,
+  SUM(ms.spend) AS total_marketing_spend,
   ROUND(SUM(o.total_amount), 2) AS total_revenue,
-  ROUND(SUM(o.total_amount) / SUM(ms.amount_spent), 2) AS marketing_roi
+  ROUND(SUM(o.total_amount) / SUM(ms.spend), 2) AS marketing_roi
 FROM marketing_spend ms
 JOIN orders o 
   ON DATE_FORMAT(o.order_date, '%Y%m') = ms.month
